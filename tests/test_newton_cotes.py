21312e8a9027ca3b784e9c53bc3d4088a910633f
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-04 15:15:11 -0700

    updated tests to include simpson and trap

---
+++ tests/test_newton_cotes.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+from src.goph420_lab01.integration import (
+    integrate_newton
+)
+
+def test_newton_trap():
+    x = np.linspace(-2, 5, 100)
+
+    # constant func
+    f0 = 5.0 * np.ones_like(x)
+    # linear func
+    f1 = -2.5 + 0.6 * x
+
+    # exp integral
+    I0_exp = f0[0] * (x[-1] - x[0])
+    # test with 2 points
+    I0_act_2 = integrate_newton(
+        [x[0], x[-1]],
+        [f0[0], f0[-1]],
+        'trap',
+    )
+    # test with all points
+    I0_act_all = integrate_newton(x, f0, 'trap')
+
+    print("Testing trapezoid rule...")
+    print(f'f(x) = {f0[0]} from x = {x[0]} to x = {x[-1]}...')
+    print(f'Expected: {I0_exp}')
+    print(f'Actual (2 data points): {I0_act_2}')
+    print(f'Actual ({len(x)} data points): {I0_act_all}')
+
+
+def test_newton_simp():
+    x = np.linspace(-3, 5, 11)
+
+    # constant func
+    f0 = 5.0 * np.ones_like(x)
+    # linear func
+    f1 = -2.5 + 0.6 * x
+
+    # exp integral
+    I0_exp = f0[0] * (x[-1] - x[0])
+    # test with 2 points
+    I0_act_2 = integrate_newton(
+        [x[0], x[-1]],
+        [f0[0], f0[-1]],
+        'simp',
+    )
+    I0_act_4 = integrate_newton(x[:4], f0[:4], 'simp')
+    # test with all points
+    I0_act_all = integrate_newton(x, f0, 'simp')
+
+    print("Testing Simpson's 1/3 rule...")
+    print(f'f(x) = {f0[0]} from x = {x[0]} to x = {x[-1]}...')
+    print(f'Expected: {I0_exp}')
+    print(f'Actual (4 data points): {I0_act_4}')
+    print(f'Actual ({len(x)} data points): {I0_act_all}')
+
+if __name__ == "__main__":
+    test_newton_trap()
+    test_newton_simp()
