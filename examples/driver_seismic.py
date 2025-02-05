75468bb86f78177893aa4cfeebcd3949b8af01f3
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-04 19:12:44 -0700

    nothing shouldve changed here lol

--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -1,13 +1,28 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    data = np.loadtxt("data/s_wave_data.txt")
+    data = np.loadtxt("C:/Users/sydne/git/goph420/goph420-w2025-lab01-stSP/data/s_wave_data.txt")
     t_data = data[:,0]
     v_data = data[:,1]
+    v2_data = v_data**2
 
-    plt.figure()
-    plt.plot(t_data, v_data, "--", label="s_wave_data")
+    plt.figure(figsize=(6, 8))
+
+    plt.subplot(2,1,1)
+    plt.plot(t_data, v_data, "-b", label="s_wave_data")
     plt.xlabel("time (s)")
     plt.ylabel("velocity (mm/s)")
     plt.legend()
-    plt.savefig("examples/s_wave_data.png")
+
+    plt.subplot(2,1,2)
+    plt.plot(t_data, v2_data, "-r", label="s_wave_data")
+    plt.xlabel("time (s)")
+    plt.ylabel("squared velocity (mm/s)")
+    plt.legend()
+
+    plt.savefig("C:/Users/sydne/git/goph420/goph420-w2025-lab01-stSP/figures/s_wave_figure.png")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
