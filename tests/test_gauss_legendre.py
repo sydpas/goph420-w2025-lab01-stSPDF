7494991242f50539ebd4ef6c19d5059e139a6ea4
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-10 14:11:09 -0700

    tested gauss legandre

--- tests/test_gauss_legendre.py
+++ tests/test_gauss_legendre.py
@@ -1,3 +1,22 @@
-def test_gauss_lagendre():
+import scipy as sp
+import numpy as np
 
-    pass
+from src.goph420_lab01.integration import (
+    integrate_gauss
+)
+
+
+def test_gauss_lagendre():
+    f = lambda x: np.sin(x)
+    lims = [0, np.pi/2]
+
+    I_exp, _ = sp.integrate.fixed_quad(f,lims[0],lims[1])  # _ is to extract only the integral
+    I_act = integrate_gauss(f, lims, npts = 3)
+
+    print("Testing Gauss Legendre rule...")
+    print(f'Using f(x) from a = {lims[0]} to b = {lims[1]}...')  # Manually print the function
+    print(f'Expected: {I_exp}')
+    print(f'Actual: {I_act}')
+
+if __name__ == "__main__":
+    test_gauss_lagendre()
