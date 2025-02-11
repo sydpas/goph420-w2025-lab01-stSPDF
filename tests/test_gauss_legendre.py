b3eb8454bfd1b8fed3f9e3a52d8a058125b90735
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-11 10:48:41 -0700

    adding to probabilty function

--- tests/test_gauss_legendre.py
+++ tests/test_gauss_legendre.py
@@ -7,15 +7,15 @@ from goph420_lab01.integration import (
 
 
 def test_gauss_legendre():
-    for n in range(1, 10):
-        f = lambda x: np.sin(x)  # function to test
-        lims = [0, np.pi/2]  # limits
+    f = lambda x: np.sin(x)  # function to test
+    lims = [0, np.pi/2]  # limits
+    print("Testing Gauss Legendre rule...")
+    print(f'Using f(x) from a = {lims[0]} to b = {lims[1]}...')  # Manually print the function
 
+    for n in range(1, 10):
         I_exp, _ = sp.integrate.fixed_quad(f, lims[0], lims[1], n = n + 1)  # _ is to extract only the integral
         I_act = integrate_gauss(f, lims, npts = 3)
 
-        print("Testing Gauss Legendre rule...")
-        print(f'Using f(x) from a = {lims[0]} to b = {lims[1]}...')  # Manually print the function
         print(f'Expected: {I_exp}')
         print(f'Actual: {I_act}')
 
