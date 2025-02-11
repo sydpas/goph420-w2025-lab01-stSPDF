2acf515a86819d6bf3ff095c5c55b629a1dce085
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-11 10:31:29 -0700

    working on gauss test

--- tests/test_gauss_legendre.py
+++ tests/test_gauss_legendre.py
@@ -1,22 +1,23 @@
 import scipy as sp
 import numpy as np
 
-from src.goph420_lab01.integration import (
+from goph420_lab01.integration import (
     integrate_gauss
 )
 
 
-def test_gauss_lagendre():
-    f = lambda x: np.sin(x)
-    lims = [0, np.pi/2]
+def test_gauss_legendre():
+    for n in range(1, 10):
+        f = lambda x: np.sin(x)  # function to test
+        lims = [0, np.pi/2]  # limits
 
-    I_exp, _ = sp.integrate.fixed_quad(f,lims[0],lims[1])  # _ is to extract only the integral
-    I_act = integrate_gauss(f, lims, npts = 3)
+        I_exp, _ = sp.integrate.fixed_quad(f, lims[0], lims[1], n = n + 1)  # _ is to extract only the integral
+        I_act = integrate_gauss(f, lims, npts = 3)
 
-    print("Testing Gauss Legendre rule...")
-    print(f'Using f(x) from a = {lims[0]} to b = {lims[1]}...')  # Manually print the function
-    print(f'Expected: {I_exp}')
-    print(f'Actual: {I_act}')
+        print("Testing Gauss Legendre rule...")
+        print(f'Using f(x) from a = {lims[0]} to b = {lims[1]}...')  # Manually print the function
+        print(f'Expected: {I_exp}')
+        print(f'Actual: {I_act}')
 
 if __name__ == "__main__":
-    test_gauss_lagendre()
+    test_gauss_legendre()
