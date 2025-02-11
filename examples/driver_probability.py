866e5c63326ef08bd0b804d88f675fc09501d48b
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-11 10:16:52 -0700

    dfjksf

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -5,11 +5,21 @@ from src.goph420_lab01.integration import (
     integrate_gauss
 )
 
-#  equation 18
-z = (x - np.mean(x)) / sigma
-lims = [np.inf, z]
-#  equation 17
-stand_normal_prob = (1/np.sqrt(2*np.pi))*np.exp((-1/2)*z**2)
-#  equation 16
-stand_gauss_norm = integrate_gauss(stand_normal_prob)
+list = [4,5,6]
+
+def prob_seismic(x):
+    x = np.asarray(x)
+    #  equation 18
+    z = (x - np.mean(x)) / np.std(x)
+    lims = [np.inf, z]
+    #  equation 17
+    stand_normal_prob = (1/np.sqrt(2*np.pi))*np.exp((-1/2)*z**2)
+    #  equation 16
+    stand_gauss_norm = integrate_gauss(stand_normal_prob, lims, 3)
+    print(stand_gauss_norm)
+
+if __name__ == "__main__":
+    prob_seismic(list)
+
+
 
