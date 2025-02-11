12e39d8c60d823dceb3af3fc2fea74e27f4b1bb5
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-11 10:01:01 -0700

    modified formulas

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -5,9 +5,11 @@ from src.goph420_lab01.integration import (
     integrate_gauss
 )
 
-#  equation 17
+#  equation 18
 z = (x - np.mean(x)) / sigma
 lims = [np.inf, z]
+#  equation 17
 stand_normal_prob = (1/np.sqrt(2*np.pi))*np.exp((-1/2)*z**2)
+#  equation 16
 stand_gauss_norm = integrate_gauss(stand_normal_prob)
 
