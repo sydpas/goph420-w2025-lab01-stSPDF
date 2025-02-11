40f245d928cb4ce8d2296439b6f1e71985305d2b
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-11 10:47:59 -0700

    modified probability

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -11,9 +11,9 @@ def prob_seismic(x):
     x = np.asarray(x)
     #  equation 18
     z = (x - np.mean(x)) / np.std(x)
-    lims = [np.inf, z]
+    lims = [-np.inf, z]
     #  equation 17
-    stand_normal_prob = (1/np.sqrt(2*np.pi))*np.exp((-1/2)*z**2)
+    stand_normal_prob = (1/np.sqrt(2*np.pi)) * np.exp((-1/2)*z**2)
     #  equation 16
     stand_gauss_norm = integrate_gauss(stand_normal_prob, lims, 3)
     print(stand_gauss_norm)
