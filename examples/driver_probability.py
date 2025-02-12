eeb72243c48bb8b12cd0a8a8e6557c084027094a
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-02-11 18:10:21 -0700

    working on prob_seismic

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -5,22 +5,40 @@ from goph420_lab01.integration import (
     integrate_gauss
 )
 
-list = [4,5,6]
+x = np.linspace(4.1, 9.9, 100)
 
 def prob_seismic(x):
-    x = np.asarray(x)
+    """
+    For part i. of question 8.
+    """
+
+    print(f'Computing the probability of a seismic event with magnitudes greater than 4...')
+    x = np.asarray(x)  # convert x to an array
+    mean = 1.5  # given in lab
+    stdev = 0.5  # given in lab
+
+    print(f'Mean: {mean}')
+    print(f'Standard deviation: {stdev}')
+
     #  equation 18
-    z = (x - np.mean(x)) / np.std(x)
-    lims = [-np.inf, z]
-    # test
-    #  equation 17
-    stand_normal_prob = (1/np.sqrt(2*np.pi)) * np.exp((-1/2)*z**2)
-    #  equation 16
+    z = (x - mean) / stdev
+
+    def stand_normal_prob(c):
+        #  equation 17
+        return (1/np.sqrt(2 * np.pi)) * np.exp((-1/2) * c ** 2)
+        #  z doesn't need to be used directly as the integration process will use it
+
+    # integration limits
+    lims = [0, np.max(z)]
+
+    #  equation 24 + using gauss
     stand_gauss_norm = integrate_gauss(stand_normal_prob, lims, 3)
-    print(stand_gauss_norm)
+
+    print(f'Standard (Gaussian) Normal: {stand_gauss_norm}')
+
 
 if __name__ == "__main__":
-    prob_seismic(list)
+    prob_seismic(x)
 
 
 
